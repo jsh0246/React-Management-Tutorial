0b213c14bef87b81bb196ca851d36141b7eca6f3
jsh0246 <jsh0246@naver.com>
2021-03-22 18:32:31 +0900

    Add Customer API Loading View

--- client/src/App.js
+++ client/src/App.js
@@ -7,47 +7,57 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 })
 
-const customers = [
-{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '한민우',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '이동기',
-  'birthday': '890512',
-  'gender': '남자',
-  'job': '디자이너'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '김현우',
-  'birthday': '950809',
-  'gender': '남자',
-  'job': '탐정'
-}
-]
+// constructor() -> componentWillMound() -> render() -> componenetDidMounut()
+// props or state changed => shouldComponentUpdate()
+// 상태관리 잘 해주면 함수 콜 알아서 해줌
+
+
 
 class App extends Component {
+
+  // state 컴포넌트내에서 변동가능함, props 변동불가능함
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  // api 서버에 접근을 해서 데이터를 받아오는 함수 컴포넌트디드마운트
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+    // this.callApi()
+    //   .then(res => this.setState({customers: res}))
+    //   .catch(err => console.log(err));
+  }
+
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,8 +74,16 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); } ) }
-        </TableBody>
+            {this.state.customers ? this.state.customers.map(c => {
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); } ) 
+             : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+             )}
+          </TableBody>
         </Table>
       </Paper>
     );
--- server.js
+++ server.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+/*
 app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
+});
+*/
+
+
+
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': '남도일',
+            'birthday': '961222',
+            'gender': '남자',
+            'job': '탐정'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': '유미란',
+            'birthday': '890512',
+            'gender': '남자',
+            'job': '대학생'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': '홍장미',
+            'birthday': '950809',
+            'gender': '남자',
+            'job': '특수요원'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
