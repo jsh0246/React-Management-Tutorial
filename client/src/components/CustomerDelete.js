32bac42023ced04cf393c0a22cd1543ef505df88
jsh0246 <jsh0246@naver.com>
2021-03-23 21:56:52 +0900

    16 doing

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Button';
 
 class CustomerDelete extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
 
     deleteCustomer(id) {
         const url = '/api/customers/' + id;
